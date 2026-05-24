4d20fe40d95b5c65008f09ae4d29dda4143ba74e
Sinja Stutterecker <sinja.stutterecker@icloud.com>
2026-05-24 09:37:12 +0200

    refactor quality update logic (AI assisted)

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,62 +1,99 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
 
+function decreaseQuality(item, amount = 1) {
+  item.quality = Math.max(0, item.quality - amount);
+}
+
+function increaseQuality(item, amount = 1) {
+  item.quality = Math.min(50, item.quality + amount);
+}
+
+function isSulfuras(item) {
+  return item.name === "Sulfuras, Hand of Ragnaros";
+}
+
+function isAgedBrie(item) {
+  return item.name === "Aged Brie";
+}
+
+function isBackstagePass(item) {
+  return item.name === "Backstage passes to a TAFKAL80ETC concert";
+}
+
+function isConjured(item) {
+  return item.name.includes("Conjured");
+}
+
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name.includes('Conjured')) {
-              this.items[i].quality = this.items[i].quality - 2;
-            } else {
-            this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
+      const item = this.items[i];
+
+      if (!isAgedBrie(item) && !isBackstagePass(item)) {
+
+        if (!isSulfuras(item)) {
+
+          if (isConjured(item)) {
+            decreaseQuality(item, 2);
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            decreaseQuality(item);
           }
+
+        }
+
+      } else {
+
+        increaseQuality(item);
+
+        if (isBackstagePass(item)) {
+
+          if (item.sellIn < 11) {
+            increaseQuality(item);
+          }
+
+          if (item.sellIn < 6) {
+            increaseQuality(item);
+          }
+
+        }
+      }
+
+      if (!isSulfuras(item)) {
+        item.sellIn--;
+      }
+
+      if (item.sellIn < 0) {
+
+        if (!isAgedBrie(item) && !isBackstagePass(item)) {
+
+          if (!isSulfuras(item)) {
+
+            if (isConjured(item)) {
+              decreaseQuality(item, 2);
+            } else {
+              decreaseQuality(item);
+            }
+
+          }
+
+        } else if (isBackstagePass(item)) {
+
+          item.quality = 0;
+
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+
+          increaseQuality(item);
+
         }
       }
     }
@@ -64,7 +101,10 @@ class Shop {
     return this.items;
   }
 }
-/*module.exports = {
+
+/*
+module.exports = {
   Item,
   Shop
-}*/
+}
+*/
