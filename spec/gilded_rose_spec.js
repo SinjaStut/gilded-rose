b6f17e372b55ea937f052112955f1fedf665fc18
Sinja Stutterecker <sinja.stutterecker@icloud.com>
2026-05-24 08:57:08 +0200

    add test for aged brie quality increase

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -13,4 +13,14 @@ describe("Gilded Rose", function() {
 
 });
 
+it("aged brie increases in quality", function () {
+  const items = [new Item("Aged Brie", 10, 20)];
+  const shop = new Shop(items);
+
+  shop.updateQuality();
+
+  expect(items[0].sellIn).toBe(9);
+  expect(items[0].quality).toBe(21);
+});
+
 });
